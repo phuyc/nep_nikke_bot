6a861dcc2e99e9c3036290fe21d2ee313c8e2ba9
phuyc <lhphuc70@gmail.com>
2023-01-25 15:11:47 +0700

    Fix timestamp

--- commands/info.js
+++ commands/info.js
@@ -6,10 +6,10 @@ const info = new EmbedBuilder()
         .setDescription('[Invite nep to your server](https://discord.com/api/oauth2/authorize?client_id=1034494981055971408&permissions=265216&scope=bot%20applications.commands)')
         .setThumbnail('https://preview.redd.it/kt3311mn5bp01.png?auto=webp&s=e420455ca8880f533a66514151a197f0f89adce6')
         .addFields(
-                { name: 'Bot Info', value: 'A bot to look up various information from the game Nikke: Goddess of Victory\n**Creator:** nepnep\n**Data provided by:** [Prydwen](https://prydwen.gg)', inline: true},
+                { name: 'Bot Info', value: 'A bot to look up various information from the game Nikke: Goddess of Victory\n**Creator:** nepnep'
+                    + '\n**Data provided by:** [Prydwen](https://prydwen.gg)\n**Art for /gacha provided by:** Normal2day#9915', inline: true},
                 { name: 'Contact Info', value: `**Discord username:**\nnepnep#1358\n**Join our Discord:** [discord.gg/prydwen](https://discord.gg/prydwen) `, inline: true}
             )
-        .setTimestamp()
         .setFooter({ text: 'nepnep#1358', iconURL: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/BE/nl/19/EP0031-CUSA03124_00-AV00000000000037/image?w=320&h=320&bg_color=000000&opacity=100&_version=00_09_000' });
             
 
@@ -18,6 +18,6 @@ module.exports = {
         .setName('info')
         .setDescription('Returns Bot info'),
     execute(interaction) {
-        interaction.editReply({ embeds: [info.setColor(randomColor())] });
+        interaction.editReply({ embeds: [info.setColor(randomColor()).setTimestamp()] });
     }
 }
