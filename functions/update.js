a8a29d47544e6b642fbe7c422748cc5c7aecc723
phuyc <lhphuc70@gmail.com>
2023-02-19 22:41:56 +0700

    Update images and handle hideSkills col

--- functions/update.js
+++ functions/update.js
@@ -6,27 +6,42 @@ const db = new Database("./nikke.db");
 async function autoUpdate() {
     // Employees
     const CurrentChar = db.prepare("SELECT name FROM characters;").all();
-    console.log(CurrentChar);
 
     let characters = await fetch("https://www.prydwen.gg/page-data/nikke/characters/page-data.json");
     let json2 = await characters.json();
     json2 = json2.result.data.allCharacters.nodes;
 
     for (let i = 0; i < json2.length; i++) {
+        // Add character to db
         if (!CurrentChar.some(char => char.name === json2[i].name)) {
 
-            // Add to DB
-            db.prepare("INSERT OR IGNORE INTO characters (name, slug, rarity) VALUES (?, ?, ?);").run(json2[i].name, json2[i].slug, json2[i].rarity);
+            // Prepare SQL
+            let update = db.prepare("INSERT OR IGNORE INTO characters (name, slug, rarity, hideSkills) VALUES (?, ?, ?, ?);");
             
             // Download image
             let cardImage = await fetch(`https://www.prydwen.gg/page-data/nikke/characters/${json2[i].slug}/page-data.json`);
             let cardJson = await cardImage.json();
-            cardJson = cardJson.result.data.currentUnit.nodes[0].cardImage.localFile.childImageSharp.gatsbyImageData.images.fallback.src;
-            let res = await fetch(`https://www.prydwen.gg${cardJson}`);
+            cardJson = cardJson.result.data.currentUnit.nodes[0];
+
+            let hideSkills = cardJson.hideSkills ? 'true' : null;
+
+            // Add to DB
+            update.run(json2[i].name, json2[i].slug, json2[i].rarity, hideSkills);
+            
+            let res = await fetch(`https://www.prydwen.gg${cardJson.cardImage.localFile.childImageSharp.gatsbyImageData.images.fallback.src}`);
 	        res.body.pipe(fs.createWriteStream(`./images/${json2[i].name}.png`));
 
             // TODO: Create full Image
         }
+
+        // Update hideSkills 
+        CurrentChar.filter(char => char.hideSkills).forEach(async char => {
+            let hs = await fetch(`https://www.prydwen.gg/page-data/nikke/characters/${char.slug}/page-data.json`);
+            let hsJson = await hs.json();
+            let check = hsJson.result.data.currentUnit.nodes[0].hideSkills;
+
+            if (!check) db.prepare("UPDATE characters SET hideSkills=? WHERE name=?;").run(null, json2[i].name);
+        })
     }
 }
 
